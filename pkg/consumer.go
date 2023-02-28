0a36653733904ea399b2046467f8e7f156af4152
Alexandre <alexr.albino@gmail.com>
2023-02-28 16:18:01 -0300

    feature: event counter test

--- pkg/consumer.go
+++ pkg/consumer.go
@@ -13,24 +13,24 @@ type Consumer interface {
 }
 
 type ConsumerWrapper struct {
-	consumer Consumer
+	Consumer Consumer
 }
 
 func (c *ConsumerWrapper) randomSleep() {
-	time.Sleep(time.Second * time.Duration(rand.Intn(30)))
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
 }
 
 func (c *ConsumerWrapper) Created(ctx context.Context, uid string) error {
 	c.randomSleep()
-	return c.Created(ctx, uid)
+	return c.Consumer.Created(ctx, uid)
 }
 
 func (c *ConsumerWrapper) Updated(ctx context.Context, uid string) error {
 	c.randomSleep()
-	return c.Updated(ctx, uid)
+	return c.Consumer.Updated(ctx, uid)
 }
 
 func (c *ConsumerWrapper) Deleted(ctx context.Context, uid string) error {
 	c.randomSleep()
-	return c.Deleted(ctx, uid)
+	return c.Consumer.Deleted(ctx, uid)
 }
